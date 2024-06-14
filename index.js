e086e76d081d62727bb49cd2e140c04f04119424
cm-saiki-kyohei <57649503+cm-saiki-kyohei@users.noreply.github>
2024-06-14 10:45:51 +0900

    grafana標準shareで飛ばす

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const app = express();
 
-// CORSを有効にする
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 app.get('/hello', (req, res) => {
-    res.send('Hello');
+  res.send('Hello');
 });
 
 app.get('/grafana', (req, res) => {
-    res.send(`
+  res.send(`
       <!DOCTYPE html>
       <html>
       <head>
@@ -29,5 +20,5 @@ app.get('/grafana', (req, res) => {
 });
 
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+  console.log('Server is running on port 8080');
 });
